b551a0121035bff700628dd6e3ba307e5b775947
yuksel.ozdemir@tokeninc.com <yuksel.ozdemir@tokeninc.com>
2020-12-19 11:03:32 +0300

    hello-03: added display names and NullPointerException test

--- hello/src/test/java/com/yukselcoding/HelloTest.java
+++ hello/src/test/java/com/yukselcoding/HelloTest.java
@@ -1,29 +1,39 @@
 package com.yukselcoding;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class HelloTest {
 
-
     @Test
+    @DisplayName("Should check hello() method in Hello class returns correctly.")
     void testHelloWorldSuccess() {
         Hello hello = new Hello();
         assertEquals("Hello, World.", hello.hello());
     }
 
     @Test
+    @DisplayName("Should check hello returns correct result against given name.")
     void testHelloWithNameSuccess() {
         String name = "Ali";
         String expected = String.format("Hello, %s.", name);
-        Hello hello = new Hello();
+        final Hello hello = new Hello();
         assertEquals(expected, hello.hello(name));
     }
 
     @Test
+    @DisplayName("Should check hello returns correct result against empty string.")
     void testHelloWithEmptyString() {
-        Hello hello = new Hello();
+        final Hello hello = new Hello();
         assertEquals("Hello, World.", hello.hello(""));
+    }
+
+    @Test
+    @DisplayName("Should check hello throws NullPointerException against null value.")
+    void testHelloWithNullValue() {
+        final Hello hello = new Hello();
+        assertThrows(NullPointerException.class, () -> hello.hello(null));
     }
 }
